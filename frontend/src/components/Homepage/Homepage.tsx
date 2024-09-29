d71aa9ce7d670b07b6fd41f5d2ffeef639c94eb1
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-09-28 20:47:27 -0700

    Edited hompage component

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -3,7 +3,8 @@
 export default function Homepage(){
     return (
         <div>
-            <h1>This is the homepage.</h1>
+            <h1>Basic Typescript Setup</h1>
+            <h2>This is the homepage :)</h2>
         </div>
     )
 }
